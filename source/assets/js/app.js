243a75b4f40bfc8372652b40d37b8b06ae2da9ff
southill <southill@gmail.com>
2018-07-16 12:32:57 +0800

    [UPDATE] 改进竖屏设计横屏状态下的适应表现

--- source/assets/js/app.js
+++ source/assets/js/app.js
@@ -14,46 +14,39 @@
 			*/
 			gm.mt = annie.MouseEvent;
 			gm.et = annie.Event;
+
 			/*
-			* 竖屏设计
 			* 全面屏 尺寸
 			* 设计尺寸 800 1280，安全范围 640 1040 居中
 			*/
-
 			$(window).on('resize',function(){
 				var _selfBox = $("#app");
+				// 当前屏幕宽高从低至高排序
 				var _selfwh = _.sortBy([_selfBox.width(),_selfBox.height()]);
+				// 当前屏幕长边换算长度
 				var _screenHeight = _selfwh[1] / (_selfwh[0] / 640);
+				//传入 stage 的 设计长边长度
 				var _desHeight = _screenHeight <= 1040 ? 1040 : _screenHeight;
-
-				self.suitHeight = -(1280 - _desHeight > 0 ? 1280 - _desHeight : 0)/2;
+				//画面居中间距
+				self.suitHeight = -(1280 - _desHeight > 0 ? 1280 - _desHeight : 0)/2 + (_screenHeight > 1280 ? (_screenHeight -1280)/2:0);
 				self.initHeight = _desHeight;
-				
+
+				if( self.page['cloading'] ){
+					// 竖屏设计
+					self.stage.desHeight = self.initHeight;
+					self.page['cloading'].y = self.suitHeight;
+
+					// 横屏设计
+					// self.stage.desWidth = self.initHeight;
+					// self.page['cloading'].x = self.suitHeight;
+
+					self.stage.resize();
+				}
 			}).trigger('resize');
+			
 			self.stage = new annie.Stage('app', 800, self.initHeight, 30, annie.StageScaleMode.FIXED_HEIGHT, 0);
-			self.stage.autoResize = true;
 
-			/*
-			* 横屏设计
-			* 全面屏 尺寸
-			* 设计尺寸 1280 800，安全范围 640 1040 居中
-			*/
-			// $(window).on('resize',function(){
-			// 	var _selfBox = $("#app");
-			// 	var _selfwh = _.sortBy([_selfBox.width(),_selfBox.height()]);
-			// 	var _screenHeight = _selfwh[1] / (_selfwh[0] / 640);
-			// 	var _desHeight = _screenHeight <= 1040 ? 1040 : _screenHeight;
-
-			// 	self.suitHeight = -(1280 - _desHeight > 0 ? 1280 - _desHeight : 0)/2 + (_screenHeight > 1280 ? (_screenHeight -1280)/2:0);
-			// 	self.initHeight = _desHeight;
-
-			// 	if( self.page['cloading'] ){
-			// 		self.stage.desWidth = self.initHeight;
-			// 		self.page['cloading'].x = self.suitHeight;
-			// 		self.stage.resize();
-			// 	}
-				
-			// }).trigger('resize');
+			// 横屏设计
 			// self.stage = new annie.Stage('app', self.initHeight, 800, 30, annie.StageScaleMode.FIXED_WIDTH, 0);
 
 			self.stage.autoSteering = true;
@@ -62,10 +55,10 @@
 					self.loadProcess(per);
 				}, function (result) {
 					self.page['cloading'] = gm.getFlaClass('cloading');
-					// 全面屏 竖屏 尺寸适配
+					// 全面屏 竖屏设计
 					self.page['cloading'].y = self.suitHeight;
 					
-					// 全面屏 横屏 尺寸适配
+					// 全面屏 横屏设计
 					// self.page['cloading'].x = self.suitHeight;
 
 					//font handlee
